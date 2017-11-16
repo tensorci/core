92e6ae4c0b7a18b6951e03a9c097fc9a2b9ce400
Ben Whittle <benwhittle31@gmail.com>
2017-11-15 22:14:55 -0800

    pseudo code for team creation endpoint

--- app/src/routes/team.py
+++ app/src/routes/team.py
@@ -1,4 +1,3 @@
-import os
 from flask_restplus import Resource
 from src.routes import namespace, api
 
@@ -7,6 +6,26 @@ from src.routes import namespace, api
 class RestfulTeam(Resource):
   """Restful Team Interface"""
 
-  @namespace.doc('example_get_request')
-  def get(self):
-    return 'Secret: {}'.format(os.environ.get('SECRET')), 200
+  @namespace.doc('create_new_team')
+  def post(self):
+    # TODO: Add more things to the DB throughout this function once you've designed DB schema
+
+    # Get user from token
+
+    # Validate user exists
+
+    # Make sure team doesn't already exist for that slug
+
+    # INSERT Team record to DB
+
+    # Create S3 Bucket for this cluster
+
+    # Create Route 53 hosted zone, returning the list of nameserver addresses associated with that zone
+
+    # Register NS records for each of the nameserver addresses returned by last command
+    #   domain: <team_slug>-cluster.domain.ai
+    #   record: <address>
+
+    # Create cluster with kops with name <team_slug>-cluster.domain.ai
+
+    return '', 201
