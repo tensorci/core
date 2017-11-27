d24d6c311957d806dbc2c1cd1d3cfcc83edb6437
Ben Whittle <benwhittle31@gmail.com>
2017-11-27 00:06:07 -0500

    removing refs to removed classes

--- app/src/services/prediction_services/__init__.py
+++ app/src/services/prediction_services/__init__.py
@@ -1,10 +1,6 @@
 from src.statuses.pred_statuses import pstatus
-from post_train_building import PostTrainBuilding
 from post_training import PostTraining
-from post_api_building import PostApiBuilding
 
 status_update_services = {
-  pstatus.DONE_BUILDING_FOR_TRAIN: PostTrainBuilding,
-  pstatus.DONE_TRAINING: PostTraining,
-  pstatus.DONE_BUILDING_FOR_API: PostApiBuilding
+  pstatus.DONE_TRAINING: PostTraining
 }
