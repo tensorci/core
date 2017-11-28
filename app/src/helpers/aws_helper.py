ce5a5ced7b848b3e08c0d6b3d9c8ad8b3b7c80ee
Ben Whittle <benwhittle31@gmail.com>
2017-11-28 12:48:34 -0600

    removed aws_helper

--- app/src/helpers/aws_helper.py
+++
@@ -1,6 +0,0 @@
-# TODO: convert this into a function that takes both zone and desired OS and then get the image
-# TODO Move all this into utils/aws.py
-# location from the AWS API
-images_map = {
-  'ubuntu-16.04': '099720109477/ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20171026.1'
-}
