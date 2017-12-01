a7b5ea5b7eafde7bc5bb78c8d033bef53e6701d0
Ben Whittle <benwhittle31@gmail.com>
2017-12-01 14:11:34 -0800

    responding with team_name_taken earlier now when trying to create team

--- app/src/helpers/user_helper.py
+++ app/src/helpers/user_helper.py
@@ -7,9 +7,6 @@ from src.helpers import decode_url_encoded_str
 
 
 def current_user():
-  # Hack for testing
-  return dbi.find_one(User, {'email': 'benwhittle31@gmail.com'})
-
   user_token = request.cookies.get('tensorci-user') or request.headers.get(auth_header_name)
 
   if not user_token:
--- app/src/routes/team.py
+++ app/src/routes/team.py
@@ -4,7 +4,9 @@ from src.helpers.user_helper import current_user
 from src.api_responses.errors import *
 from src.api_responses.success import *
 from psycopg2 import IntegrityError
-from src import logger
+from slugify import slugify
+from src import logger, dbi
+from src.models import Team
 from src.services.team_services.create_team import CreateTeam
 
 create_team_model = api.model('Team', {
@@ -24,12 +26,12 @@ class RestfulTeam(Resource):
     if not user:
       return UNAUTHORIZED
 
+    if dbi.find_one(Team, {'slug': slugify(api.payload['name'], separator='-', to_lower=True)}):
+      return TEAM_NAME_TAKEN
+
     try:
       create_team_svc = CreateTeam(name=api.payload['name'], owner=user)
       create_team_svc.perform()
-    except IntegrityError:
-      logger.error('Team already exists for name: {}'.format(api.payload['name']))
-      return TEAM_NAME_TAKEN
     except BaseException as e:
       logger.error('Error creating Team(name={}) and TeamUser(user.email={}): {}'.format(
         api.payload['name'], user.email, e))
