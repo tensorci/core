d039310a24a35f3e896ca8078b81fe8474f31ddf
Ben Whittle <benwhittle31@gmail.com>
2017-12-03 17:51:05 -0800

    annotating migrate.py

--- app/migrate.py
+++ app/migrate.py
@@ -1,6 +1,9 @@
+"""
+Not currently in use, but will be helpful once you figure out when you wanna call this.
+"""
 from src import db
 import os
 
-db.engine.execute('DROP TABLE IF EXISTS alembic_version;')
-
-os.system('python manage db init && python manage db migrate && python manage db upgrade')
+if __name__ == '__main__':
+  db.engine.execute('DROP TABLE IF EXISTS alembic_version;')
+  os.system('python manage db init && python manage db migrate && python manage db upgrade')
