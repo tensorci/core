7d563b0a5000b235aae61a8f67ab77b214a29a79
Ben Whittle <benwhittle31@gmail.com>
2017-12-24 01:18:49 -0500

    refactoring retrain_watcher.py; adding it as option for CORE_ROLE

--- app/retrain_watcher.py
+++ app/retrain_watcher.py
@@ -1,81 +1,76 @@
 import os
-from src import db, logger, dbi
+from src import db, logger
 from sqlalchemy.orm import joinedload
-from src.models import Prediction, Deployment
+from src.models import Prediction
 from src.utils.dataset_db import record_count
-from src.utils import clusters
-from src.deploys.build_server_deploy import BuildServerDeploy
-from src.deploys.train_deploy import TrainDeploy
-from src.utils.job_queue import job_queue
+from src.services.deployment_services.retrain_model import RetrainModel
 from time import sleep
-from src.helpers.deployment_statuses import ds
+
+
+def get_predictions():
+  # Get all predictions and eager load datasets
+  predictions = db.session.query(Prediction) \
+    .options(joinedload(Prediction.datasets)) \
+    .filter_by(is_destroyed=False).all()
+
+  logger.info('Found {} predictions'.format(len(predictions)))
+
+  return predictions
+
+
+def check_if_needs_retrain(prediction):
+  # Get datasets for prediction
+  datasets = prediction.datasets
+
+  # Prediction must have a dataset in order to be retrained
+  if not datasets:
+    return
+
+  # Get deployments for prediction, ordered by most recent
+  deployments = prediction.ordered_deployments()
+
+  # Prediction must have made at least one deployment before being retrained
+  if not deployments:
+    return
+
+  # Get the latest deployment
+  latest_deployment = deployments[0]
+
+  # We're loosely sticking to the 1-dataset-per-prediction constraint for now
+  dataset = datasets[0]
+
+  # If retrain_step_size hasn't been set, the prediction isn't eligible for retraining
+  if not dataset.retrain_step_size:
+    return
+
+  # Find the current number of records in the dataset table
+  table = dataset.table()
+  curr_record_count = record_count(table=table)
+
+  # curr_record_count shouldn't ever be falsey (will always be a positive integer)
+  if not curr_record_count:
+    logger.warn('Current record count returned falsey for table {}'.format(table))
+    return
+
+  # Check if it's time to retrain the model based on the number of new records in the dataset
+  if (curr_record_count - dataset.last_train_record_count) >= dataset.retrain_step_size:
+    logger.info('Time to retrain model for prediction, {}. Record count diff: {} --> {}'.format(
+      prediction.slug, dataset.last_train_record_count, curr_record_count))
+
+    # Retrain the model
+    retrain_svc = RetrainModel(prediction=p,
+                               latest_deployment=latest_deployment,
+                               dataset=dataset,
+                               curr_record_count=curr_record_count)
+    retrain_svc.perform()
 
 
 def watch(intv=60):
   while True:
-    # Get all predictions and eager load datasets
-    predictions = db.session.query(Prediction)\
-      .options(joinedload(Prediction.datasets))\
-      .filter_by(is_destroyed=False).all()
-
-    logger.info('Found {} predictions'.format(len(predictions)))
+    predictions = get_predictions()
 
     for p in predictions:
-      datasets = p.datasets
-
-      if not datasets:
-        continue
-
-      deployments = p.ordered_deployments()
-
-      if not deployments:
-        continue
-
-      latest_deployment = deployments[0]
-
-      # We're loosely sticking to the 1-dataset-per-prediction constraint for now
-      dataset = datasets[0]
-
-      if not dataset.retrain_step_size:
-        continue
-
-      # Find the current number of records in the dataset table
-      table = dataset.table()
-      curr_record_count = record_count(table=table)
-
-      if not curr_record_count:
-        logger.warn('Current record count returned falsey for table {}'.format(table))
-        continue
-
-      # Check if it's time to retrain the model based on the number of new records in the dataset
-      if curr_record_count - dataset.last_train_record_count >= dataset.retrain_step_size:
-        logger.info('Time to retrain model for prediction, {}.'
-                    '\nLast trained with: {} records'
-                    '\nNow training with: {} records'.format(p.slug, dataset.last_train_record_count, curr_record_count))
-
-        # Clone the latest deployment
-        deployment = dbi.create(Deployment, {
-          'prediction': p,
-          'sha': latest_deployment.sha
-        })
-
-        # Schedule a training build if the latest deployment hasn't already uploaded a build for this SHA.
-        if ds.statuses.index(latest_deployment.status) < ds.statuses.index(ds.DONE_BUILDING_FOR_TRAIN):
-          deployer = BuildServerDeploy(deployment_uid=deployment.uid,
-                                       build_for=clusters.TRAIN,
-                                       update_prediction_model=True)
-        else:
-          # A build for this SHA has already been uploaded, so just deploy directly to the training cluster.
-          deployment = dbi.update(deployment, {'status': ds.DONE_BUILDING_FOR_TRAIN})
-
-          deployer = TrainDeploy(deployment_uid=deployment.uid,
-                                 update_prediction_model=True)
-
-        # Schedule deploy
-        job_queue.add(deployer.deploy, meta={'deployment': deployment.uid})
-
-        # Update the dataset with it's new current record count
-        dbi.update(dataset, {'last_train_record_count': curr_record_count})
+      check_if_needs_retrain(p)
 
     sleep(intv)
 
---
+++ app/src/services/deployment_services/retrain_model.py
@@ -0,0 +1,46 @@
+from src import dbi, logger
+from src.models import Deployment
+from src.utils import clusters
+from src.deploys.build_server_deploy import BuildServerDeploy
+from src.deploys.train_deploy import TrainDeploy
+from src.utils.job_queue import job_queue
+from src.helpers.deployment_statuses import ds
+
+
+class RetrainModel(object):
+
+  def __init__(self, prediction=None, latest_deployment=None, dataset=None, curr_record_count=None):
+    self.prediction = prediction
+    self.latest_deployment = latest_deployment
+    self.dataset = dataset
+    self.curr_record_count = curr_record_count
+
+  def perform(self):
+    # Create new deployment with the same SHA as the latest deployment.
+    new_deployment = dbi.create(Deployment, {
+      'prediction': self.prediction,
+      'sha': self.latest_deployment.sha
+    })
+
+    # Get proper deployer class, based on if SHA has already been built.
+    deployer = self.get_deployer(new_deployment)
+
+    # Schedule deploy
+    job_queue.add(deployer.deploy, meta={'deployment': new_deployment.uid})
+
+    # Update the dataset with it's new current record count
+    dbi.update(self.dataset, {'last_train_record_count': self.curr_record_count})
+
+  def get_deployer(self, deployment):
+    # Schedule a training build if the latest deployment hasn't already uploaded a build for this SHA.
+    if ds.statuses.index(self.latest_deployment.status) < ds.statuses.index(ds.DONE_BUILDING_FOR_TRAIN):
+      deployer = BuildServerDeploy(deployment_uid=deployment.uid,
+                                   build_for=clusters.TRAIN,
+                                   update_prediction_model=True)
+    else:
+      # A build for this SHA has already been uploaded, so just deploy directly to the training cluster
+      # and update this deployment's status to DONE_BUILDING_FOR_TRAIN.
+      deployment = dbi.update(deployment, {'status': ds.DONE_BUILDING_FOR_TRAIN})
+      deployer = TrainDeploy(deployment_uid=deployment.uid, update_prediction_model=True)
+
+    return deployer
