88e940cd3bd954a79745dbf990d3b06fb64fe6ed
Ben Whittle <benwhittle31@gmail.com>
2017-12-25 06:51:41 -0500

    NOTE about future plans for /api/oauth endpoint

--- app/src/routes/integration.py
+++ app/src/routes/integration.py
@@ -12,71 +12,90 @@ from urlparse import urlparse
 config = get_config()
 
 
-@namespace.route('/oauth/<string:slug>')
-class OAuth(Resource):
-  """
-  OAuth callback endpoint for integrations
-  """
-  @namespace.doc('oauth_callback')
+# **NOTE**: This is incorrect...only uncomment when you start needing individual user identification
+# with the GitHub App or want to do Login with Github on TensorCI. Also, this handler would create a
+# UserIntegration, not a PredictionIntegration.
+# @namespace.route('/oauth/<string:slug>')
+# class OAuth(Resource):
+#   """
+#   OAuth callback endpoint for integrations
+#   """
+#   @namespace.doc('oauth_callback')
+#   def get(self, slug):
+#     # TODO: evaluate fact that you're publicly exposing an N+1 query for this public endpoint
+#     integration = dbi.find_one(Integration, {'slug': slug})
+#
+#     if not integration:
+#       return INTEGRATION_NOT_FOUND
+#
+#     args = dict(request.args.items())
+#     temp_code = args.get('code')
+#     state = args.get('state')
+#
+#     if not temp_code:
+#       logger.error('No temporary "code" arg provided in OAuth redirect for integration, {}'.format(slug))
+#       return INVALID_OAUTH_TEMP_CODE
+#
+#     if not state:
+#       logger.error('No "state" arg provided in OAuth redirect for integration, {}'.format(slug))
+#       return INVALID_OAUTH_STATE_VALUE
+#
+#     prediction = dbi.find_one(Prediction, {'uid': state})
+#
+#     if not prediction:
+#       return PREDICTION_NOT_FOUND
+#
+#     try:
+#       parsed_url = urlparse(request.url)
+#       redirect_uri = '{}://{}{}'.format(parsed_url.scheme, parsed_url.netloc, parsed_url.path)
+#
+#       token_swap_svc = OAuthTokenExchange(integration=integration,
+#                                           temp_code=temp_code,
+#                                           redirect_uri=redirect_uri,
+#                                           state=state)
+#       token_swap_svc.perform()
+#     except BaseException as e:
+#       logger.error('Error executing oauth_token_exchange for prediction(uid={}), integration(slug={}): {}'.format(
+#         state, slug, e))
+#       return OAUTH_TOKEN_SWAP_FAILED
+#
+#     if not token_swap_svc.access_token:
+#       logger.error('Access token still empty after requesting (prediction(uid={}), integration(slug={}))'.format(
+#         state, slug))
+#       return OAUTH_TOKEN_SWAP_FAILED
+#
+#     try:
+#       pred_integration, is_new = dbi.upsert(PredictionIntegration, {
+#         'prediction': prediction,
+#         'integration': integration
+#       })
+#     except BaseException as e:
+#       logger.error('Error upserting PredictionIntegration (prediction(uid={}), integration(slug={}): {}'.format(
+#         state, slug, e))
+#       return PREDICTION_INTEGRATION_UPSERT_FAILED
+#
+#     if pred_integration.api_key != token_swap_svc.access_token:
+#       dbi.update(pred_integration, {'api_key': token_swap_svc.access_token})
+#
+#     # TODO: I think this is a user token, so we'll probably save this in a new UserIntegration model
+#
+#     return PREDICTION_INTEGRATION_CREATION_SUCCESS
+
+
+@namespace.route('/installed/<string:slug>')
+class Installed(Resource):
   def get(self, slug):
-    # TODO: evaluate fact that you're publicly exposing an N+1 query for this public endpoint
     integration = dbi.find_one(Integration, {'slug': slug})
 
     if not integration:
       return INTEGRATION_NOT_FOUND
 
     args = dict(request.args.items())
-    temp_code = args.get('code')
-    state = args.get('state')
+    installation_id = args.get('installation_id')
 
-    if not temp_code:
-      logger.error('No temporary "code" arg provided in OAuth redirect for integration, {}'.format(slug))
-      return INVALID_OAUTH_TEMP_CODE
 
-    if not state:
-      logger.error('No "state" arg provided in OAuth redirect for integration, {}'.format(slug))
-      return INVALID_OAUTH_STATE_VALUE
 
-    prediction = dbi.find_one(Prediction, {'uid': state})
-
-    if not prediction:
-      return PREDICTION_NOT_FOUND
-
-    try:
-      parsed_url = urlparse(request.url)
-      redirect_uri = '{}://{}{}'.format(parsed_url.scheme, parsed_url.netloc, parsed_url.path)
-
-      token_swap_svc = OAuthTokenExchange(integration=integration,
-                                          temp_code=temp_code,
-                                          redirect_uri=redirect_uri,
-                                          state=state)
-      token_swap_svc.perform()
-    except BaseException as e:
-      logger.error('Error executing oauth_token_exchange for prediction(uid={}), integration(slug={}): {}'.format(
-        state, slug, e))
-      return OAUTH_TOKEN_SWAP_FAILED
-
-    if not token_swap_svc.access_token:
-      logger.error('Access token still empty after requesting (prediction(uid={}), integration(slug={}))'.format(
-        state, slug))
-      return OAUTH_TOKEN_SWAP_FAILED
-
-    try:
-      pred_integration, is_new = dbi.upsert(PredictionIntegration, {
-        'prediction': prediction,
-        'integration': integration
-      })
-    except BaseException as e:
-      logger.error('Error upserting PredictionIntegration (prediction(uid={}), integration(slug={}): {}'.format(
-        state, slug, e))
-      return PREDICTION_INTEGRATION_UPSERT_FAILED
-
-    if pred_integration.api_key != token_swap_svc.access_token:
-      dbi.update(pred_integration, {'api_key': token_swap_svc.access_token})
-
-    # TODO: I think this is a user token, so we'll probably save this in a new UserIntegration model
-
-    return PREDICTION_INTEGRATION_CREATION_SUCCESS
+    return {}, 200
 
 
 @namespace.route('/webhook/<string:slug>')
@@ -91,8 +110,9 @@ class Webhook(Resource):
     if not integration:
       return INTEGRATION_NOT_FOUND
 
-    logger.info('{} webhook heard for prediction'.format(slug, prediction))
+    logger.info('{} webhook heard'.format(slug))
     logger.info('Payload: {}'.format(api.payload))
     logger.info('Headers: {}'.format(request.headers))
 
     return {}, 200
+
