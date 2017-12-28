c46fcd46d16c4e8beb683179a8605f71dc11b6cc
Ben Whittle <benwhittle31@gmail.com>
2017-12-28 16:55:33 -0500

    removing team routes

--- app/src/routes/__init__.py
+++ app/src/routes/__init__.py
@@ -4,8 +4,7 @@ api = Api(version='0.1', title='TensorCI Core API')
 namespace = api.namespace('api')
 
 # Add all route handlers here:
-# from team import *
-# from provider_user import *
+from provider_user import *
 # from deployment import *
 # from dataset import *
 # from prediction import *
