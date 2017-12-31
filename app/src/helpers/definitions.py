404597b0dcab11d4c295cf72d41a9af07ae0f608
Ben Whittle <benwhittle31@gmail.com>
2017-12-31 17:08:35 -0500

    removing integrations/ dir

--- app/src/helpers/definitions.py
+++ app/src/helpers/definitions.py
@@ -1,9 +1,7 @@
 import os
 
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-
 source_dir = base_dir + '/src'
-
 configs_dir = source_dir + '/configs'
 
 auth_header_name = 'TensorCI-Api-Token'
