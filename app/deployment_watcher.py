03b4927544b73e9d91aeba21d3a874faf175dc52
Ben Whittle <benwhittle31@gmail.com>
2018-01-20 19:47:13 -0800

    switching to full publishes of entire deployment stages

--- app/deployment_watcher.py
+++ app/deployment_watcher.py
@@ -2,7 +2,7 @@ import json
 from src.utils.pyredis import redis
 from src import dbi
 from src.models import Deployment
-from src.helpers import deployment_helper
+from src.helpers.deployment_helper import current_stage, format_stages
 from src.utils import pubsub
 from src.helpers.definitions import deploy_update_queue
 
@@ -19,24 +19,34 @@ def handle_update(item):
   if not deployment_uid or not stage:
     return
 
-  # Get the format stage function for this stage from deployment_helper
-  format_stage_func_name = 'format_{}_stage'.format(stage)
-
-  # Make sure the function even exists...
-  if not hasattr(deployment_helper, format_stage_func_name):
-    return
-
   # Get deployment for uid
   deployment = dbi.find_one(Deployment, {'uid': deployment_uid})
 
-  if not deployment:
-    return
+  payload = {
+    'current_stage': current_stage(deployment),
+    'stages': format_stages(deployment)
+  }
 
-  # Get the most recent formatted stage info for this stage/deployment
-  stage_info = getattr(deployment_helper, format_stage_func_name)(deployment)
+  pubsub.publish(channel=deployment_uid, data=payload)
 
-  # Broadcast this update to any clients listening
-  pubsub.publish(channel=deployment_uid, data={stage: stage_info})
+  # Get the format stage function for this stage from deployment_helper
+  # format_stage_func_name = 'format_{}_stage'.format(stage)
+  #
+  # # Make sure the function even exists...
+  # if not hasattr(deployment_helper, format_stage_func_name):
+  #   return
+  #
+  # # Get deployment for uid
+  # deployment = dbi.find_one(Deployment, {'uid': deployment_uid})
+  #
+  # if not deployment:
+  #   return
+  #
+  # # Get the most recent formatted stage info for this stage/deployment
+  # stage_info = getattr(deployment_helper, format_stage_func_name)(deployment)
+  #
+  # # Broadcast this update to any clients listening
+  # pubsub.publish(channel=deployment_uid, data={stage: stage_info})
 
 
 def watch():
