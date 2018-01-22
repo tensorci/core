845bebf9b55521e738cdec2dae25bd44cfd5be45
Ben Whittle <benwhittle31@gmail.com>
2018-01-22 00:55:11 -0800

    using kubectl directly now to fuck with envs

--- app/src/services/env_services/update_deploy_env.py
+++ app/src/services/env_services/update_deploy_env.py
@@ -1,85 +1,34 @@
 from src import dbi, logger
-from src.models import Repo
-from kubernetes import client, config
-from src.utils import clusters
-from src.helpers.deployment_helper import api_deploy_envs
-from src.services.cluster_services.export_cluster import ExportCluster
+from src.models import Repo, Env
+from src.utils import kubectl
 
 
 class UpdateDeployEnv(object):
 
-  def __init__(self, repo_uid=None):
+  def __init__(self, repo_uid=None, env_uids=None):
     self.repo_uid = repo_uid
-    self.repo = None
-    self.cluster = None
-    self.container_name = '{}-{}'.format(clusters.API, repo_uid)
-    self.image = None
-    self.namespace = 'default'
+    self.env_uids = env_uids or []
 
   def perform(self):
-    # Set attrs reliant on db queries (and blow up if records not found)
-    self.set_db_reliant_attrs()
+    repo = dbi.find_one(Repo, {'uid': self.repo_uid})
+    cluster_name = repo.team.cluster.name
+    deploy_name = repo.deploy_name
 
-    # Ensure cluster/context exists in KUBECONFIG
-    context_exported = ExportCluster(cluster=self.cluster).perform()
+    if not deploy_name:
+      logger.error('Not updating envs for Repo(uid={}) -- No deploy_name on repo.'.format(self.repo_uid))
+      return
 
-    if not context_exported:
-      raise BaseException('Failure exporting context for Cluster(id={}).'.format(self.cluster.id))
+    envs = dbi.find_all(Env, {'uid': self.env_uids})
 
-    # Get the new deploy spec with updated envs
-    body = self.new_deploy_body()
+    if not envs:
+      logger.error('Not updating envs for Repo(uid={}) -- No envs found for uids: {}.'.format(
+        self.repo_uid, self.env_uids))
+      return
 
-    # Get api client instance
-    api_client = config.new_client_from_config(context=self.cluster.name)
-    api = client.ExtensionsV1beta1Api(api_client=api_client)
+    success = kubectl.set_envs(deployment_name=deploy_name,
+                               envs=envs,
+                               context=cluster_name,
+                               cluster=cluster_name)
 
-    # Patch the deployment with the new spec
-    api.patch_namespaced_deployment(self.repo.deploy_name,
-                                    namespace=self.namespace,
-                                    body=body)
-
-  def new_deploy_body(self):
-    envs = api_deploy_envs(self.repo, cluster=self.cluster)
-
-    for env in self.repo.api_envs():
-      if env.name not in envs:
-        envs[env.name] = env.value
-
-    updated_envs = [client.V1EnvVar(name=n, value=v) for n, v in envs.iteritems()]
-
-    return {
-      'spec': {
-        'template': {
-          'spec': {
-            'containers': [
-              {
-                'name': self.container_name,
-                'image': self.image,
-                'envs': updated_envs
-              }
-            ]
-          }
-        }
-      }
-    }
-
-  def set_db_reliant_attrs(self):
-    self.repo = dbi.find_one(Repo, {'uid': self.repo_uid})
-
-    if not self.repo:
-      raise BaseException('Not updating deploy env -- No repo exists for uid {}.'.format(self.repo_uid))
-
-    self.cluster = self.repo.team.cluster
-
-    # Find latest API deployment for this repo and get its commit SHA
-    deployment = None
-    for dep in self.repo.ordered_deployments():
-      if not dep.failed and dep.intent_to_serve():
-        deployment = dep
-        break
-
-    if not deployment:
-      raise BaseException('Not updating deploy env -- No unfailed deployment with intent to serve '
-                          'exists for Repo(uid={})'.format(self.repo_uid))
-
-    self.image = '{}/{}:{}'.format(self.repo.image_repo_owner, self.container_name, deployment.commit.sha)
+    if not success:
+      logger.error('Failed to update envs for Repo(uid={}): {}'.format(self.repo_uid, self.env_uids))
--- app/src/utils/kubectl.py
+++ app/src/utils/kubectl.py
@@ -21,3 +21,33 @@ def annotate(resource=None, resource_name=None, labels=None, context=None, clust
             ['--context', context, '--cluster', cluster]
 
   return bool_response_cmd(command)
+
+
+def set_envs(deployment_name=None, envs=None, context=None, cluster=None):
+  envs = envs or []
+
+  if not envs:
+    return True
+
+  formatted_envs = ['='.join([env.name, env.value]) for env in envs]
+
+  command = ['kubectl', 'set', 'env', 'deployment/{}'.format(deployment_name)] + \
+            formatted_envs + \
+            ['--context', context, '--cluster', cluster]
+
+  return bool_response_cmd(command)
+
+
+def remove_envs(deployment_name=None, env_names=None, context=None, cluster=None):
+  env_names = env_names or []
+
+  if not env_names:
+    return True
+
+  formatted_envs = ['{}-'.format(name) for name in env_names]
+
+  command = ['kubectl', 'set', 'env', 'deployment/{}'.format(deployment_name)] + \
+            formatted_envs + \
+            ['--context', context, '--cluster', cluster]
+
+  return bool_response_cmd(command)
